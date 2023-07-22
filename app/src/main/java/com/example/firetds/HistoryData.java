9b5daac39b0726e0b066f8f8c8e0964b63f4f232
juliette2000 <chen.juliette2000@gmail.com>
2023-07-22 15:20:43 -0400

    button
    button

---
+++ app/src/main/java/com/example/firetds/HistoryData.java
@@ -0,0 +1,24 @@
+package com.example.firetds;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HistoryData extends AppCompatActivity {
+    private TextView Data;
+    public Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Water test");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_history_data);
+        Data=findViewById(R.id.Data);
+        Data.setText("Data");
+
+    }
+}
